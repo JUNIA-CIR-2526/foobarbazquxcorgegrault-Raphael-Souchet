df7a59581ef27409531f4f001319933adf30911b
Raphael <raphael.souchet.85@gmail.com>
2025-09-19 19:08:02 +0200

    Class corge and grault generated

---
+++ src/main/java/com/jad/Grault.java
@@ -0,0 +1,14 @@
+package com.jad;
+
+public class Grault {
+    private Foo foo;
+
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
+}
